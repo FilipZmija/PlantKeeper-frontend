b1f30e0edee3cec5dfbe8f7fc8f32b899aa87462
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-06-03 23:11:07 +0200

    Add config files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const { red, grey } = require("@mui/material/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "ui-sans-serif", "system-ui"],
+      },
+      colors: {
+        "dark-green": "#88AB8E",
+        green: "#AFC8AD",
+        lightgreen: "#E1F0DA",
+        beige: "#EEE7DA",
+        "light-brown": "#E8DFCA",
+        "light-mint": "#f0f2eb",
+        "light-grey": "#F2F1EB",
+        "text-green": "#1A4D2E",
+      },
+    },
+  },
+  plugins: [],
+};
