58cd1edd523a781f846c02c675224630a9ac9593
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-06-04 23:34:51 +0200

    add types for plant

---
+++ src/types/plants.ts
@@ -0,0 +1,13 @@
+export interface Plant {
+  name: string;
+  apiId?: string;
+  commonName?: string;
+  availability?: string;
+  lightTolerated?: string;
+  lightIdeal?: string;
+  temperatureMax?: number;
+  temperatureMin?: number;
+  watering?: string;
+  climat?: string;
+  img?: string;
+}
