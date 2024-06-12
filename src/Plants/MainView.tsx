dab071ac1ebd9d8d6dda9552771cf4c07478fcc8
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-06-12 22:51:02 +0200

    tests

--- src/Plants/MainView.tsx
+++ src/Plants/MainView.tsx
@@ -1,45 +1,28 @@
-import React from "react";
-import UploadImage from "../Identification/UploadImage";
+import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
-import Carousel from "./Reusable/Carousel";
-import { TOwnedPlant } from "../types/plants";
-import { getDate } from "../helpers/date";
+import axios from "axios";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { setOwnedPlants } from "../redux/ownedPlantSlice";
 
 export default function MainView() {
-  const ownedPlant: TOwnedPlant = {
-    id: 1,
-    name: "Yucca elephantipes",
-    commonName: "Spineless yucca, Palm lily",
-    lastWatered: getDate(-5).toString(),
-    lastTransplanted: getDate(-5).toString(),
-    lastFertilized: getDate(-10).toString(),
-    soliMoisture: 200,
-    desiredMoisture: 100,
-    wateringType: "off",
-    plantId: 44,
-    image:
-      "https://filipzmijewski.blob.core.windows.net/img/2e5be930-5273-4b10-805a-3a8849cba24d.jpg",
-    userId: 1,
-    createdAt: "2024-06-02T15:18:41.070Z",
-    updatedAt: "2024-06-02T15:18:41.070Z",
-    plant: {
-      id: 44,
-      name: "Yucca elephantipes",
-      apiId: "a5178737-1b07-56b8-8667-0be3477fef48",
-      commonName: "Spineless yucca, Palm lily",
-      avaibility: "Regular",
-      lightTolerated: "Strong light ( 21,500 to 3,200 lux/2000 to 300 fc)",
-      lightIdeal: "Full sun (+21,500 lux /+2000 fc )",
-      temperatureMax: 30,
-      temperatureMin: 12,
-      watering: "Keep moist between watering & Water when soil is half dry",
-      climat: "Subtropical",
-    },
-  };
+  const dispatch = useAppDispatch();
+  const { ownedPlants } = useAppSelector((state) => state.ownedPlant);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/ownedplant/user/1`
+      );
+      console.log(response.data);
+      dispatch(setOwnedPlants([...response.data, ...response.data]));
+    })();
+  }, [dispatch]);
 
   return (
-    <>
-      <PlantCard ownedPlant={ownedPlant} />
-    </>
+    <div className="flex flex-row flex-wrap justify-center sm:mx-5 md:mx-10 lg:mx-24 xl:mx-36 2xl:mx-48">
+      {ownedPlants.map((item, index) => (
+        <PlantCard key={item.id} plantIndex={index} />
+      ))}
+    </div>
   );
 }
